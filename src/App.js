193d988acaa829cf2485295b86d56d2ce3b61265
volkovich <volkoviysash@gmail.com>
2023-02-27 14:05:15 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator-box">
+        <div className="displays">
+          <div className="upper-display">123</div>
+          <div className="lower-display">123</div>
+        </div>
+        <div className="numpad">
+          <div className="operation num-AC">
+            <button className="operation-btn">AC</button>
+          </div>
+          <div className="operation">
+            <button className="operation-btn">/</button>
+          </div>
+          <div className="operation">
+            <button className="operation-btn">x</button>
+          </div>
+          <div className="">
+            <button>7</button>
+          </div>
+          <div className="">
+            <button>8</button>
+          </div>
+          <div className="">
+            <button>9</button>
+          </div>
+          <div className="operation">
+            <button className="operation-btn">-</button>
+          </div>
+          <div className="">
+            <button>4</button>
+          </div>
+          <div className="">
+            <button>5</button>
+          </div>
+          <div className="">
+            <button>6</button>
+          </div>
+          <div className="operation">
+            <button className="operation-btn">+</button>
+          </div>
+          <div className="">
+            <button>1</button>
+          </div>
+          <div className="">
+            <button>2</button>
+          </div>
+          <div className="">
+            <button>3</button>
+          </div>
+          <div className="num-zero">
+            <button>0</button>
+          </div>
+          <div className="">
+            <button>.</button>
+          </div>
+          <div className="operation equal">
+            <button className="operation-btn equal">=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
